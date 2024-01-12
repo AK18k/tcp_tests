696d79ead48f65db36af968e1a33191ebb8a3c5a
AK18k <avi18000@gmail.com>
2024-01-12 11:17:03 +0200

    hello workd with cmake.

---
+++ build/CMakeFiles/tcp_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tcp_test.dir/main.cpp.o"
+  "tcp_test"
+  "tcp_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tcp_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/avan2101/git_repos/tcp_tests
+# Build directory: /home/avan2101/git_repos/tcp_tests/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
